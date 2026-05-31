19bf70a6316b06e03b8f3773f38c5c61bd957156
Karthik200619 <23eg112e53@anurag.edu.in>
2026-05-31 15:22:29 +0530

    Update nodemailer.js

--- backend/config/nodemailer.js
+++ backend/config/nodemailer.js
@@ -1,22 +1,31 @@
 import nodemailer from 'nodemailer'
 
-
 const transporter = nodemailer.createTransport({
-    secure:true,
-    host:'smtp.gmail.com',
-    port:465,
-    auth:{
-        user:'23eg112e53@anurag.edu.in',
-        pass:'ayxaffegmroulzwo'
+    secure: true,
+    host: 'smtp.gmail.com',
+    port: 465,
+    auth: {
+        user: '23eg112e53@anurag.edu.in',
+        pass: 'ayxaffegmroulzwo'
     }
 })
+
 async function sendemail(to, sub, msg) {
+    try {
+        const info = await transporter.sendMail({
+            from: '23eg112e53@anurag.edu.in',
+            to,
+            subject: sub,
+            html: msg
+        })
 
-    await transporter.sendMail({
-        to:to,
-        subject: sub,
-        html: msg
-    })
+        console.log('MAIL SENT:', info.response)
 
+    } catch (err) {
+
+        console.error('MAIL ERROR:', err)
+
+    }
 }
-export default sendemail;
+
+export default sendemail
